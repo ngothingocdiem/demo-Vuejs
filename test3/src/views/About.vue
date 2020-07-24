b83d325ba5e99ae2aa3b7e13bded539af6777d6c
thien414 <lethanhthien414@gmail.com>
2020-07-24 14:47:33 +0700

    add file

---
+++ test3/src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <AddStudent/>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import AddStudent from '@/components/addStudents/addStudent.vue'
+
+export default {
+  name: 'About',
+  components: {
+    AddStudent
+  }
+}
+</script>
---
+++ test3/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <filterStudent/>
+    <Student/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Student from '@/components/Students/Student'
+import filterStudent from '@/components/filterStudents/fileterStudent'
+
+export default {
+  name: 'Home',
+  components: {
+    Student,
+    filterStudent
+  }
+}
+</script>
