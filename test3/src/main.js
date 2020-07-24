67edb0993468e148fa21cb5663e1fc99b6405a0c
chuyên đề phát triển web 1 <ngothingocdiem98@gmail.com>
2020-07-24 14:19:33 +0700

    diem-24-7-2020

--- test3/src/main.js
+++ test3/src/main.js
@@ -1,7 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store/store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store/store';
+
+/**
+ * diem create bootstrap & bootstrap-vue 24-7-2020
+ */
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'; // diem create 24/7/2020
+
+// Import the styles directly. (Or you could add them via script tags.)
+import 'bootstrap/dist/css/bootstrap.css'; // diem 24/7/2020
+import 'bootstrap-vue/dist/bootstrap-vue.css'; // diem 24/7/2020
+
+Vue.use(BootstrapVue); // diem 24/7/2020
 
 Vue.config.productionTip = false
 
